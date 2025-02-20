984fdf2f95986f4010f691ca60aab3ec410e5141
jdupreltu <jdupre@ltu.edu>
2025-02-20 16:40:44 -0500

    Add project files.

---
+++ WebcamHW/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+
+namespace WebcamHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emguPictureBox = new System.Windows.Forms.PictureBox();
+            this.binaryPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // emguPictureBox
+            // 
+            this.emguPictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.emguPictureBox.Location = new System.Drawing.Point(29, 31);
+            this.emguPictureBox.Name = "emguPictureBox";
+            this.emguPictureBox.Size = new System.Drawing.Size(800, 524);
+            this.emguPictureBox.TabIndex = 0;
+            this.emguPictureBox.TabStop = false;
+            // 
+            // binaryPictureBox
+            // 
+            this.binaryPictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.binaryPictureBox.Location = new System.Drawing.Point(857, 31);
+            this.binaryPictureBox.Name = "binaryPictureBox";
+            this.binaryPictureBox.Size = new System.Drawing.Size(800, 524);
+            this.binaryPictureBox.TabIndex = 1;
+            this.binaryPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1692, 589);
+            this.Controls.Add(this.binaryPictureBox);
+            this.Controls.Add(this.emguPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private PictureBox binaryPictureBox;
+    }
+}
+
---
+++ WebcamHW/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WebcamHW
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // create the capture object and processing thread
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                // frame maintenance
+                Mat frame = _capture.QueryFrame();
+
+                // resize to PictureBox aspect ratio
+                int newHeight = (frame.Size.Height * emguPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(emguPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                // display the image in the PictureBox
+                emguPictureBox.Image = frame.ToBitmap();
+
+                /* Your code for grayscaling and binary threshold-ing the image should go here */
+                CvInvoke.CvtColor(frame, frame, ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(frame, frame, 150, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+                binaryPictureBox.Image = frame.ToBitmap();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // terminate the image processing thread to avoid orphaned processes
+            _captureThread.Abort();
+        }
+    }
+}
